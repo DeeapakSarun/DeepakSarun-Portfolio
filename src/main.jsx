0d5626d55f2fb9575011277dbf172020df88693e
deeapak Sarun <dyuvachandran@slu.edu>
2025-02-26 14:17:36 -0600

    footer Line added

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Threads from './components/Threads.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <div style={{ width: '100%', height: '600px', position: 'relative' }}>
+  <Threads
+    amplitude={0.1}
+    distance={0.1}
+    enableMouseInteraction={true}
+  />
+</div>
   </StrictMode>,
 )
