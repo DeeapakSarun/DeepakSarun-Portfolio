92b335ae0de22d941d92a5f4caf167834acb8856
deeapak Sarun <dyuvachandran@slu.edu>
2025-03-02 22:35:24 -0600

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,10 @@ function App() {
               minFontSize={70}
           />
         </div>
-
+        <div className="section2"></div>
+        <div className="section3"></div>
+        <div className="section4"></div>
+        <div className="section5"></div>
       </section>
     </div>
 
